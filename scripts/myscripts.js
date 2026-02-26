92211e7a78fb750a22dcd0b3510304430d94b2e5
bahaeldin <bahaa.ahmed.eng@gmail.com>
2026-02-26 00:39:34 -0800

    adds images and carousel function

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -1,8 +1,22 @@
+// function showAlt(x) {
+//   document.getElementById("alttext").innerHTML=x.alt;
+// }
 
-function showAlt(x) {
-  document.getElementById("alttext").innerHTML=x.alt;
+// function hideAlt(x) {
+//   document.getElementById("alttext").innerHTML="";
+// }
+
+const slides = document.querySelectorAll(".slide");
+let index = 0;
+
+function showSlide(i) {
+    slides.forEach(slide => slide.classList.remove("active"));
+    slides[i].classList.add("active");
 }
 
-function hideAlt(x) {
-  document.getElementById("alttext").innerHTML="";
-}
+showSlide(index);
+
+setInterval(() => {
+    index = (index + 1) % slides.length;
+    showSlide(index);
+}, 3000);
